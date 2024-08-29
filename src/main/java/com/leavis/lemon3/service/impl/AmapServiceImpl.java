71a6fcdcd29ea2be1e1a370ce8e31d7c6c94e562
paynejlli <paynejlli@tencent.com>
2024-08-29 15:45:40 +0800

    feat:将Gson实例化为类的成员变量，避免每次调用时创建新实例，添加了异常处理逻辑，捕获并记录异常信息。

--- src/main/java/com/leavis/lemon3/service/impl/AmapServiceImpl.java
+++ src/main/java/com/leavis/lemon3/service/impl/AmapServiceImpl.java
@@ -4,8 +4,11 @@ import com.google.gson.Gson;
 import com.leavis.lemon3.client.AmapClient;
 import com.leavis.lemon3.client.config.AmapConfig;
 import com.leavis.lemon3.client.model.WeatherForecastResponse;
+import com.leavis.lemon3.exception.BizException;
+import com.leavis.lemon3.rsp.ResultCodeEnum;
 import com.leavis.lemon3.service.AmapService;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,7 @@ import org.springframework.stereotype.Service;
  * @Description: 高德
  * @Date: 2024/8/20 11:48
  */
+@Slf4j
 @Service
 public class AmapServiceImpl implements AmapService {
 
@@ -22,11 +26,17 @@ public class AmapServiceImpl implements AmapService {
     @Resource
     private AmapClient amapClient;
 
+    private final Gson gson = new Gson();
+
     @Cacheable(cacheNames = "weatherCache", unless = "#result == null || #result.trim().isEmpty()")
     @Override
     public String getWeatherInfo(String cityCode) {
-        WeatherForecastResponse weatherForecastResponse = amapClient.weatherInfo(amapConfig.getApiKey(), cityCode);
-        Gson gson = new Gson();
-        return gson.toJson(weatherForecastResponse);
+        try {
+            WeatherForecastResponse weatherForecastResponse = amapClient.weatherInfo(amapConfig.getApiKey(), cityCode);
+            return gson.toJson(weatherForecastResponse);
+        } catch (Exception e) {
+            log.error("Failed to get weather info for city code: {}", cityCode, e);
+            throw new BizException(ResultCodeEnum.FAILED, e);
+        }
     }
 }
